7d0181fd5775dbc5edfddf44022101c21b92589c
DyingToFall <shonzero@gmail.com>
2017-11-29 21:25:15 -0800

    The change in a comment

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,8 @@ package com.dyingtofall.notitiacadere;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+//This is a change. Does this work for anyone? Can you see me?
 public class MainActivity extends AppCompatActivity
 {
 
